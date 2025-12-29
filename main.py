a0a86b86328259049f2614e077e085c495bf8bb4
sergeial1972-user <243555155+sergeial1972-user@users.noreply.github.com>
2025-12-29 12:05:43 +0300

    full rework

--- main.py
+++ main.py
@@ -1,49 +1,16 @@
 #imports
-import logging
-import configparser
 import requests
+from pathlib import Path
+from dotenv import load_dotenv
 
-# Configure logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+#dotenv
+load_dotenv()
 
-# Create handlers
-file_handler = logging.FileHandler('app.log')  # file handler
-file_handler.setLevel(logging.INFO)
+def logs_read():
+    log_files = []
+    log_dir = Path('var/log')
+    if log_dir.exists() and log_dir.is_dir():
 
-stream_handler = logging.StreamHandler()  # console handler
-stream_handler.setLevel(logging.INFO)
 
-# Create formatters and add to handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-stream_handler.setFormatter(formatter)
 
-# Add handlers to the logger
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
-
-# Read config from file
-config = configparser.ConfigParser()
-config.read("config.ini")
-
-endpoint = config['connection']['endpoint']
-sending_frequency = config['sender']['sending_frequency']
-
-def log_read():
-    with open("/var/log/syslog", "r", encoding="utf-8") as syslog:
-        syslog_content = syslog.read()
-    logger.info("Syslog successfully read")
-    return syslog_content
-
-def log_send():
-    url = endpoint
-    content = log_read()
-    response = requests.post(url, data={'file_content': content})
-    if response.ok:
-        logger.info("Log successfully sent")
-    else:
-        logger.error(f"Failed to send log: {response.status_code}")
-
-if __name__ == "__main__":
-    log_send()
+logs_read()`
